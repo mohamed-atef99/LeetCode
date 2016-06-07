4b160d55c48f730ea8b93a9c892b812515a654e2
mohamed-atef99 <muhammad.atef.saleh@gmail.com>
2016-06-07 21:24:56 +0300

    [100. Same Tree][Accepted]committed by mohamed atef

---
+++ 100-Same-Tree/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+     public boolean isSameTree(TreeNode p, TreeNode q) 
+	 {
+	      if(p==null&&q==null)
+		  {
+		    return true;
+		  } 
+	      else if(p!=null&&q!=null)
+		  {
+		     if(p.val==q.val)
+			 {
+			   if(isSameTree(p.left,q.left))
+			   {
+			      return isSameTree(p.right,q.right);
+			   }
+			   else
+			   {
+			     return false;
+			   }
+			 }
+			 else
+			 {
+			   return false;
+			 }
+		  }
+	      else
+		  {
+		    return false;
+		  }	  
+	 }
+}
