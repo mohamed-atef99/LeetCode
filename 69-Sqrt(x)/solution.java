f510baa945799a05c7d50d749ee4f89471755037
mohamed-atef99 <muhammad.atef.saleh@gmail.com>
2016-07-04 16:49:44 +0300

    [69. Sqrt(x)][Accepted]committed by mohamed atef

---
+++ 69-Sqrt(x)/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int mySqrt(int a) 
+    {
+        int start=1;
+	    int end =a;
+	    while(start<=end)
+	    {
+	        int mid=(start+end)/2;
+	        if(mid==a/mid)
+    		{
+    			return mid;
+    		}
+    		else if(mid>a/mid)
+    		{
+    			end=mid-1;
+    		}
+    		else
+    		{
+    			start=mid+1;
+    		}
+	    }
+	    return end;
+    }
+}
