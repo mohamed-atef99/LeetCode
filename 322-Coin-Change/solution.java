0538b2761b616702ed0d271c4ddf05e38232d9f3
mohamed-atef99 <muhammad.atef.saleh@gmail.com>
2016-07-15 00:45:18 +0300

    [322. Coin Change][Accepted]committed by mohamed atef

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,16 +1,29 @@
 public class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;             
-        int[] dp = new int[amount + 1];  
-        Arrays.fill(dp, max);  
-        dp[0] = 0;   
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                }
-            }
-        }
-        return dp[amount] > amount ? -1 : dp[amount];
+    public int coinChange(int[] bills, int amount)
+    {
+        if(bills==null)
+          return -1;
+         if(amount==0)
+           return 0;
+           
+    	int [] MinCache=new int[amount];
+    	
+    	for(int i=1;i<=amount;i++)
+    	{
+    		int min=Integer.MAX_VALUE;
+    		
+    		for(int bill:bills)
+    		{
+    		    int remaining=i-bill;
+    			if(remaining>0&&MinCache[remaining-1]>0)
+    			  min=Math.min(MinCache[remaining-1]+1, min);
+    			else if(i-bill==0)
+    				min=1;
+    		}
+    		MinCache[i-1]=min==Integer.MAX_VALUE?-1:min;
+    	}
+    	
+    	return MinCache[amount-1];
+    
     }
 }
